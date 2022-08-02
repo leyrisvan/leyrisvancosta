18197a070964a317fad2bd2ee9e7400873e0d08b
leyrisvan <47274695+leyrisvan@users.noreply.github.com>
2022-08-02 01:02:31 -0300

    Add files via upload

---
+++ scikit-image-examples/imagem/mydata.py
@@ -0,0 +1,31 @@
+from skimage import io
+
+root = "C:/Users/leyrisvan/Processamento-de-imagens/scikit-image-examples/models/"
+
+def coelho():
+    imagem = io.imread(root + 'coelho.png')
+    return imagem
+
+def gorro():
+    imagem = io.imread(root + 'gorro.png')
+    return imagem
+
+
+def boneco():
+   imagem = io.imread(root + 'boneco.png')
+    return imagem
+
+
+def flamengo():
+   imagem = io.imread(root + 'flamengo.png')
+    return imagem
+
+
+def noel():
+    imagem= io.imread(root + 'noel.png')
+    return imagem
+
+def hiena():
+   imagem = io.imread(root + 'hiena.png')
+    return imagem
+
